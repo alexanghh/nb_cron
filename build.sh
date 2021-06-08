29d365cd12b2fca417b5880d7f4147d69b279677
alexanghh <alexanghh@gmail.com>
2021-06-08 21:10:58 +0800

    Cleaned up files and meta.yml

--- build.sh
+++
@@ -1,4 +0,0 @@
-python setup.py develop
-jupyter nbextension install nb_cron --py --sys-prefix --symlink
-jupyter nbextension enable nb_cron --py --sys-prefix
-jupyter serverextension enable nb_cron --py --sys-prefix
