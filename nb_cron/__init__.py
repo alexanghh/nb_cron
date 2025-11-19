8c8413e3d0454cf52cba3f4fc2771a8d8e48663d
Alex Ang HH <alexanghh@gmail.com>
2025-11-19 00:11:53 +0000

    Updated versioneer to 0.29.

--- nb_cron/__init__.py
+++ nb_cron/__init__.py
@@ -7,9 +7,8 @@
 #############################################################################
 # flake8: noqa
 from .handlers import load_jupyter_server_extension
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
 
 def _jupyter_nbextension_paths():
     return [dict(section="notebook",
